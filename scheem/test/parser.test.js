8d68c2c111a154a595ac3c98562aa75f21b28d26
Byron McMullen <byronsm@gmail.com>
2012-04-28 14:56:44 -0700

    added support for random whitespace

---
+++ scheem/test/parser.test.js
@@ -0,0 +1,18 @@
+var PEG = require('pegjs');
+var assert = require('assert');
+var fs = require('fs');
+
+var data = fs.readFileSync('../parser.peg', 'utf-8');
+var parse = PEG.buildParser(data).parse;
+
+
+assert.deepEqual( parse(""), []);
+assert.deepEqual( parse("atom"), "atom");
+assert.deepEqual( parse("+"), "+");
+assert.deepEqual( parse("(a b c)"), ["a", "b", "c"] );
+assert.deepEqual( parse(" (a b c)"), ["a", "b", "c"] );
+assert.deepEqual( parse(" (a b c)"), ["a", "b", "c"] );
+assert.deepEqual( parse("(a   b c)"), ["a", "b", "c"] );
+assert.deepEqual( parse("(a b c)"), ["a", "b", "c"] );
+assert.deepEqual( parse("(+ x 3)"), ["+", "x", "3"] );
+assert.deepEqual( parse("(+ 1 (f x 3 y))"), ["+", "1", ["f", "x", "3", "y"]]);
